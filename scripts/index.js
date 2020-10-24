f34147e7cb70bb74a70fee03a5b9d0f1daf30784
Tatiana Ryzhova <tryzhova93@gmail.com>
2020-10-24 21:28:42 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-const editButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 
-const profileName = document.querySelector('.profile__name');
-const profileTitle = document.querySelector('.profile__title');
+let profileName = document.querySelector('.profile__name');
+let profileTitle = document.querySelector('.profile__title');
 
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close-button');
-const popupForm = document.querySelector('.popup__form');
-const popupNameField = document.querySelector('.popup__input_type_name');
-const popupTitleField = document.querySelector('.popup__input_type_title');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let popupForm = document.querySelector('.popup__form');
+let popupNameField = document.querySelector('.popup__input_type_name');
+let popupTitleField = document.querySelector('.popup__input_type_title');
 
 function showPopup() {
   popup.classList.add('popup_opened');
